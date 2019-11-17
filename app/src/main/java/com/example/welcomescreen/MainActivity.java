7bfd2429219c7d20d45404e8b4e73e149c9c8a9f
Kawsar Amin <kawsar.amin845@gmail.com>
2019-11-18 01:03:06 +0600

    original files

---
+++ app/src/main/java/com/example/welcomescreen/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.welcomescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
